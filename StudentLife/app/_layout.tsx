df2c3ec19eaf2b26d6efdd955e69639d80ebfee5
dyeaman05 <danielyeaman05@gmail.com>
2025-10-11 14:43:14 -0500

    add tabs and blank calendar screen
    totally created a unique and original javascript code for a tab bar and didn't pull it from the expo docs. calendar was original tho.

--- StudentLife/app/_layout.tsx
+++ StudentLife/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ StudentLife/app/calendar.tsx
@@ -0,0 +1,16 @@
+import { ThemedText } from "@/app-example/components/themed-text";
+import { Text, View } from "react-native";
+
+export default function Calendar() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <Text>Calendar</Text>
+    </View>
+  );
+}
