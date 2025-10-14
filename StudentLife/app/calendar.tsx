bd75f6dd24d2de639966ea7de2eb12989ad4b7f9
dyeaman05 <danielyeaman05@gmail.com>
2025-10-14 14:08:18 -0500

    adds selection dot to calendar dates
    when a user taps a date, the app now shows a blue dot over the date highlight the user's suggestion.

--- StudentLife/app/calendar.tsx
+++ StudentLife/app/calendar.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View } from "react-native";
-import { Calendar } from 'react-native-calendars';
+import { Calendar, LocaleConfig} from 'react-native-calendars';
 
 export default function CalendarScreen() {
   const today = new Date().toISOString().split('T')[0];
+  
+  const [selected, setSelected] = useState('');
+
   return (
     <View>
       <Calendar
       current = {today}
-      onDayPress = {(day) => {console.log (day)}}
+      onDayPress = {(day) => {
+        console.log (day), setSelected(day.dateString);
+      }}
+
+      markedDates={{
+        [selected]: {selected: true, disableTouchEvent: true}
+      }}
       />
     </View>
   );
